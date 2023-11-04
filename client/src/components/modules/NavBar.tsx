bd22b265d728133db89906a4e7e5d588376813f9
Peter-Nie2003 <PeterNie2003@outlook.com>
2023-11-03 23:56:05 -0400

    revise on navbar

--- client/src/components/modules/NavBar.tsx
+++ client/src/components/modules/NavBar.tsx
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faCalendar } from '@fortawesome/free-solid-svg-icons';
 import UR_img from '../ur.jpeg';
+import './NavBar.css';
+import  '../utils/utilities.css';
 // function have two buttons, one for back to home page, one for go to calender page
 function NavBar() {
   const logoStyle = {
@@ -13,10 +15,12 @@ function NavBar() {
     height: 'auto',    // This will maintain the aspect ratio
   };
   return (
-    <Navbar bg="light" data-bs-theme="light">
+    <Navbar bg="primary" data-bs-theme="primary">
       <Container>
-        <img src={UR_img} style={logoStyle}></img>
-        <Navbar.Brand href="#home">UR List</Navbar.Brand>
+        <div>
+          <img className="circular-image" src={UR_img} style={logoStyle}>
+        </img></div>
+        <Navbar.Brand className="u-bold setcolor" href="#home">UR List</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">
             <FontAwesomeIcon icon={faHome} />
@@ -24,6 +28,7 @@ function NavBar() {
           <Nav.Link href="/calendar">
             <FontAwesomeIcon icon={faCalendar} />
           </Nav.Link>
+          <div className="text">A clever todo list help you sort your calendar</div>
         </Nav>
       </Container>
     </Navbar>
